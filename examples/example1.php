402816741d954f66eb9f3627841cabd42ba1a511
Server <server@server.com>
2014-02-06 17:49:18 -0200

    Add a example

---
+++ examples/example1.php
@@ -0,0 +1,18 @@
+<?php
+    require_once '../config/autoload.php';
+
+    //Validating a Email
+    
+    use Validator\Email;
+    use Validator\NotEmpty;
+    
+    $email = 'gamjj74@hotmail.com';
+    
+    $validator = new NotEmpty;
+    $validator->setSucessor( new Email );
+    
+    try{
+        $validator->validate( $email );
+    }catch( Exception\InvalidArgumentException $e ){
+        echo '<b>Error:</b> ' . $e->getMessage() . '<br /> <b>On test:</b> ' . $e->getInvalidParameterName();
+    }
